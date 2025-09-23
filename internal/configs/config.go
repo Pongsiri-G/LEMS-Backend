a9d5c54151659731e54d2e252774ae4cf21d179f
PerTrakanjan <thanakrit.tra@ku.th>
2025-09-24 00:49:21 +0700

    feat(config): read .env and init database

--- internal/configs/config.go
+++ internal/configs/config.go
@@ -1,7 +1,31 @@
 package configs
 
-type Config struct{}
+import (
+	"github.com/caarlos0/env/v11"
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog/log"
+)
+
+type Config struct {
+	Port 				int16 	`env:"PORT"`
+
+	DatabaseHost		string	`env:"DATABASE_HOST"`
+	DatabaseName     	string	`env:"DATABASE_NAME"`
+	DatabaseUsername 	string	`env:"DATABASE_USERNAME"`
+	DatabasePassword 	string	`env:"DATABASE_PASSWORD"`
+	DatabasePort     	int16	`env:"DATABASE_PORT"`
+}
 
 func NewConfig() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Warn().Msg("No .env file found or error loading it.")
+	}
+
+	config := &Config{}
+
+	if err := env.Parse(config); err != nil {
+		log.Panic().Err(err).Msg("Failed to parse environment variables")
+	}
+
 	return nil
 }
--- internal/infrastructure/database/pgsql.go
+++ internal/infrastructure/database/pgsql.go
@@ -1,1 +1,30 @@
 package database
+
+import (
+	"fmt"
+
+	"github.com/471-68-SE-Classroom/p1-final-project-backend-lems-ya/internal/configs"
+	"github.com/rs/zerolog/log"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitPostgres(cfg *configs.Config) {
+	connection := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Bangkok",
+		cfg.DatabaseHost, 
+		cfg.DatabaseUsername, 
+		cfg.DatabasePassword, 
+		cfg.DatabaseName,
+		cfg.DatabasePort,
+	)
+
+	db, err := gorm.Open(postgres.Open(connection), &gorm.Config{})
+	if err != nil {
+		log.Printf("Failed to connect to DB: %v", err)
+	}
+
+	DB = db
+}
