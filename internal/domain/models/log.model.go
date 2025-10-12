0acf66eb57256a7356be0fd3b9ad6c075cd9f710
Pongsiri-G <Pongsiri__G@hotmail.com>
2025-10-12 21:02:22 +0700

    feat(log): fix struct

--- internal/domain/models/log.model.go
+++ internal/domain/models/log.model.go
@@ -9,8 +9,8 @@ import (
 
 type Log struct {
 	LogID      uuid.UUID     `db:"log_id" gorm:"type:uuid;primaryKey"`
-	UserID     *uuid.UUID    `db:"user_id" gorm:"type:uuid;foreignKey:UserID;references:UserID"`
+	UserID     uuid.UUID     `db:"user_id" gorm:"type:uuid;foreignKey:UserID;references:UserID"`
 	LogType    enums.LogType `db:"log_type"`
-	LogMessage string        `db:"log_message"`
+	LogMessage *string       `db:"log_message"`
 	CreatedAt  time.Time     `db:"created_at"`
 }
