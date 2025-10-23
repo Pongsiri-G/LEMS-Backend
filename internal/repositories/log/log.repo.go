9bd0dfdafea0324213526b76b35119fa4461a36e
Pannathorn Suetrong <pannathorn.su@gmail.com>
2025-10-23 15:01:54 +0700

    feat(log) : add implementation for log repository

--- internal/repositories/log/log.repo.go
+++ internal/repositories/log/log.repo.go
@@ -3,6 +3,7 @@ package log
 import (
 	"context"
 
+	"github.com/471-68-SE-Classroom/p1-final-project-backend-lems-ya/internal/domain/enums"
 	"github.com/471-68-SE-Classroom/p1-final-project-backend-lems-ya/internal/domain/models"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
@@ -25,12 +26,27 @@ func NewLogRepository(db *gorm.DB) Repository {
 
 // CreateBorrowLog implements Repository.
 func (r *RepositoryImpl) CreateBorrowLog(ctx context.Context, userID uuid.UUID, itemID uuid.UUID) error {
-	panic("unimplemented")
+	logMessage := userID.String() + ":" + itemID.String()
+	logEntry := &models.Log{
+		LogID:      uuid.New(),
+		UserID:     userID,
+		LogType:    enums.LogTypeBorrow,
+		LogMessage: &logMessage,
+	}
+	return r.db.WithContext(ctx).Create(logEntry).Error
+
 }
 
 // CreateReturnLog implements Repository.
 func (r *RepositoryImpl) CreateReturnLog(ctx context.Context, userID uuid.UUID, itemID uuid.UUID) error {
-	panic("unimplemented")
+	logMessage := userID.String() + ":" + itemID.String()
+	logEntry := &models.Log{
+		LogID:      uuid.New(),
+		UserID:     userID,
+		LogType:    enums.LogTypeReturn,
+		LogMessage: &logMessage,
+	}
+	return r.db.WithContext(ctx).Create(logEntry).Error
 }
 
 func (r *RepositoryImpl) Create(ctx context.Context, log *models.Log) error {
