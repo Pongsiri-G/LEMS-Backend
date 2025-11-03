162cdca3100b25a2b987a3750c447f2674dfc01e
PerTrakanjan <138445215+PerTrakanjan@users.noreply.github.com>
2025-11-03 14:37:05 +0700

    Hotfix/auth (#56)
    * refactor(user): rename MyInfo to FindByID and update related handlers
    
    * feat(noti): enhance enqueue to unqine queue for a user
    
    * fix(auth): fix oathu signin
    
    * fix(ws): correct a client to connect specificly
    
    * feat(borrowq): add new functions in repository
    
    * feat(borrowq): add a get my borrow queue service
    
    * feat(borrowq): add cancel queue service
    
    * feat(borrowq): add queue cancellation
    
    * feat(middleware): verify user status
    
    * refactor(middleware): verify user correct role and able to revoke

--- internal/middlewares/rbac.middleware.go
+++ internal/middlewares/rbac.middleware.go
@@ -5,34 +5,44 @@ import (
 
 	"github.com/471-68-SE-Classroom/p1-final-project-backend-lems-ya/internal/configs"
 	"github.com/471-68-SE-Classroom/p1-final-project-backend-lems-ya/internal/domain/enums"
+	"github.com/471-68-SE-Classroom/p1-final-project-backend-lems-ya/internal/services/user"
 	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog/log"
 )
 
 type rbacMiddleware struct {
 	configs *configs.Config
+	userService user.UserService
 }
 
 type RbacMiddleware interface {
-	Middleware(next echo.HandlerFunc, roles ...string) echo.HandlerFunc
+	Middleware(next echo.HandlerFunc, roles ...enums.UserRole) echo.HandlerFunc
 }
 
-func NewRbacMiddleware(configs *configs.Config) RbacMiddleware {
+func NewRbacMiddleware(configs *configs.Config, userService user.UserService) RbacMiddleware {
 	return &rbacMiddleware{
 		configs: configs,
+		userService: userService,
 	}
 }
 
-func (a *rbacMiddleware) Middleware(next echo.HandlerFunc, roles ...string) echo.HandlerFunc {
+func (a *rbacMiddleware) Middleware(next echo.HandlerFunc, roles ...enums.UserRole) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		userRole, ok := c.Get(string(enums.UserRoleContextKey)).(string)
+		userID, ok := c.Get(string(enums.UserIDContextKey)).(string)
 		if !ok {
-			return c.JSON(http.StatusForbidden, map[string]string{"message": "User role not found"})
+			return c.JSON(http.StatusForbidden, map[string]string{"message": "User id not found"})
+		}
+
+		user, err := a.userService.FindByID(c.Request().Context(), userID)
+		if err != nil {
+			log.Err(err).Msg("failed to find user")
+			return c.JSON(http.StatusForbidden, map[string]string{"message": err.Error()})
 		}
 
 		hasPermission := false
 
 		for _, role := range roles {
-			if userRole == role {
+			if user.UserRole == role {
 				hasPermission = true
 				break
 			}
